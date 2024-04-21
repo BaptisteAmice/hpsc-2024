6a935a6bc127fd754f90ddcc8a0c5a1f5cba8aa6
Baptiste AMICE <amice.bap@gmail.com>
2024-04-21 23:33:17 +0900

    openmp: ex3

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -15,14 +15,20 @@ int main() {
   std::vector<int> bucket(range,0); 
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
+
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
+  std::vector<int> offset_temp(range,0);
+#pragma omp parallel
+  {
+    for (int j=1; j<range; j<<=1) {
+#pragma omp for
+      for (int i=0; i<range; i++) 
+        offset_temp[i] = offset[i];
+#pragma omp for
+      for (int i=j; i<range; i++) {
+        offset[i] += offset_temp[i-j] + bucket[i-j];
+      } 
+    } 
   }
 
   for (int i=0; i<n; i++) {
